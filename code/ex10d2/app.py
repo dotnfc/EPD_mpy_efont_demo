05e0cf4483d008e5f853a328b9fbc8e8d1acb341
dotnfc <dotnfc@users.noreply.github.com>
2023-12-20 18:43:07 +0800

    Update app.py
    drop test code

--- code/ex10d2/app.py
+++ code/ex10d2/app.py
@@ -5,14 +5,12 @@ import sys
 import machine
 import logging as log
 from .ui_calendar import uiCalendar
-from .test import test
 
 def main():
     log.setLevel(log.INFO)
     log.info("EFore Demo Started")
     
     try:
-        test()
         uiCalendar().start()
     except Exception as e:
         log.exception(e,'Exception in main.py')
